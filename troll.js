e01de3c05bb2497acec3105bbc7f75f84868346a
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-05-25 01:37:16 -0700

    update style

--- troll.js
+++ troll.js
@@ -7,15 +7,18 @@ zen.register_commands(
     [
         {
             name: 'ls',
-            description: 'Trolls the user for saying ls'
+            description: 'Trolls the user for saying ls.'
         }, {
-            name: 'irssi',
-            description: 'When someone mentions irssi, suggests weechat.'
+            name: '!source',
+            description: 'Links to the source.'
+        }, {
+            name: '!ping',
+            description: 'Pong.'
         }
     ]
 )
 
-var privmsg = zen.filter({verison: 1, type: 'privmsg'})
+var privmsg = zen.filter({version: 1, type: 'privmsg'})
 privmsg.on('data', function(msg) {
     if (/^ls$/.test(msg.data.message)) {
         zen.send_privmsg(msg.data.channel,
@@ -28,7 +31,7 @@ directed.on('data', function(msg) {
     var who = ['whoareyou', 'who are you?', 'source']
     if (/^ping$/i.test(msg.data.message)) {
         zen.send_privmsg(msg.data.channel, msg.data.sender + ': pong!')
-    } else if (who.indexOf(msg.data.message) != -1) {
+    } else if (who.indexOf(msg.data.message) !== -1) {
         zen.redis.get('zenircbot:nick', function(err, nick) {
             zen.send_privmsg(msg.data.channel,
                              'I am ' + nick + ', an instance of ' +
@@ -37,4 +40,3 @@ directed.on('data', function(msg) {
         })
     }
 })
-
